e21196dbab510dad63177c277af00a59cfbcf931
Fernando W S Furtado <feh.wilinando@gmail.com>
2018-04-22 20:18:35 -0300

    [Fix] Team to add total expected people field - my bad =/

--- src/main/java/br/com/caelum/feel/feedback/forms/TeamForm.java
+++ src/main/java/br/com/caelum/feel/feedback/forms/TeamForm.java
@@ -2,13 +2,19 @@ package br.com.caelum.feel.feedback.forms;
 
 import br.com.caelum.feel.feedback.domain.companyteams.models.CompanyTeam;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 public class TeamForm {
     private Long id;
 
     @NotEmpty
     private String name;
+
+    @NotNull
+    @Min(1)
+    private Integer totalExpectedPeople;
 
     public Long getId() {
         return id;
@@ -26,20 +32,21 @@ public class TeamForm {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return "TeamForm{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+    public Integer getTotalExpectedPeople() {
+        return totalExpectedPeople;
+    }
+
+    public void setTotalExpectedPeople(Integer totalExpectedPeople) {
+        this.totalExpectedPeople = totalExpectedPeople;
     }
 
     public void fillFrom(CompanyTeam companyTeam) {
         id = companyTeam.getId();
         name = companyTeam.getName();
+        totalExpectedPeople = companyTeam.getTotalExpectedPeople();
     }
 
     public CompanyTeam toEntity() {
-        return new CompanyTeam(id, name);
+        return new CompanyTeam(id, name, totalExpectedPeople);
     }
 }
