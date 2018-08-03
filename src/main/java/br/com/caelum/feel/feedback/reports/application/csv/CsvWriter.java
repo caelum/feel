b9b0995bc1e835ff3c621060f2521306f9c2e6f1
asouza <alots.ssa@gmail.com>
2018-08-03 19:02:25 -0300

    csv com ponto virgula

--- src/main/java/br/com/caelum/feel/feedback/reports/application/csv/CsvWriter.java
+++ src/main/java/br/com/caelum/feel/feedback/reports/application/csv/CsvWriter.java
@@ -12,20 +12,22 @@ import org.supercsv.prefs.CsvPreference;
 @Component
 public class CsvWriter {
 
+	private static CsvPreference preference = new CsvPreference.Builder('"', ';', "\n").build();
+
 	public String write(List<?> lines, String[] header, CellProcessor[] processors) {
 		StringWriter writer = new StringWriter();
-		try (ICsvBeanWriter beanWriter = new CsvBeanWriter(writer,
-				CsvPreference.EXCEL_PREFERENCE)) {
+
+		try (ICsvBeanWriter beanWriter = new CsvBeanWriter(writer, preference)) {
 
 			beanWriter.writeHeader(header);
 			for (final Object line : lines) {
 				beanWriter.write(line, header, processors);
 			}
-				
+
 		} catch (Exception exception) {
 			throw new RuntimeException(exception);
 		}
-		
+
 		return writer.toString();
 	}
 }
