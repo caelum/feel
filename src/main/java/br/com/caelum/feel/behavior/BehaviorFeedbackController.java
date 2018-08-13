e0f90def5938ce46a2fbd23284f7ad100c38359a
asouza <alots.ssa@gmail.com>
2018-08-13 14:22:13 -0300

    link errado do site

--- src/main/java/br/com/caelum/feel/behavior/BehaviorFeedbackController.java
+++ src/main/java/br/com/caelum/feel/behavior/BehaviorFeedbackController.java
@@ -33,7 +33,7 @@ public class BehaviorFeedbackController {
 
 		redirectAttributes.addFlashAttribute("msg",
 				"Seu feedback foi registrado com sucesso. "
-						+ " Para que você siga essa conversa, copie o link http://people.caelu.com.br/behavior/anonimous/timeline/"
+						+ " Para que você siga essa conversa, copie o link http://people.caelum.com.br/behavior/anonimous/timeline/"
 						+ newFeedback.getHash()+". Acesse esse endereço dentro das próximas 48 horas e a gente já vai ter um retorno inicial para você. ");
 		
 		return "redirect:/behavior/feedback/anonimous/form";
