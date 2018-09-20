81a794e4c51c9e77cb8f30880a50681ae282f0ae
asouza <alots.ssa@gmail.com>
2018-09-20 12:45:53 -0300

    exibindo numero de participantes e total de respostas

--- src/main/java/br/com/caelum/feel/feedback/reports/application/views/ReportPerTeamAnswerTable.java
+++ src/main/java/br/com/caelum/feel/feedback/reports/application/views/ReportPerTeamAnswerTable.java
@@ -1,6 +1,8 @@
 package br.com.caelum.feel.feedback.reports.application.views;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.List;
 import java.util.Optional;
 
@@ -15,17 +17,40 @@ public class ReportPerTeamAnswerTable {
 	public ReportPerTeamAnswerTable(List<ReportPerTeamAnswer> answers) {
 		this.answers = answers;
 	}
-	
+
+	/**
+	 * 
+	 * @param otherTeam time que deve ser buscado
+	 * @param otherQuestion questao que deve ser buscada
+	 * @return o valor em percentual. Ex: 0.2. Isso siginifica 20%. 
+	 */
 	public BigDecimal percentCount(CompanyTeam otherTeam, Question otherQuestion) {
-		Optional<ReportPerTeamAnswer> found = answers.stream().filter(answer -> answer.belongs(otherTeam, otherQuestion)).findFirst();
-		
+		Optional<ReportPerTeamAnswer> found = findAnswer(otherTeam, otherQuestion);
+
 		return found.map(answer -> answer.getPercentAnswer()).orElse(BigDecimal.ZERO);
 	}
-	
+
+	private Optional<ReportPerTeamAnswer> findAnswer(CompanyTeam otherTeam,
+			Question otherQuestion) {
+		return answers.stream().filter(answer -> answer.belongs(otherTeam, otherQuestion))
+				.findFirst();
+	}
+
 	public BigDecimal percentValue(CompanyTeam otherTeam, Question otherQuestion) {
-		Optional<ReportPerTeamAnswer> found = answers.stream().filter(answer -> answer.belongs(otherTeam, otherQuestion)).findFirst();
-		
+		Optional<ReportPerTeamAnswer> found = findAnswer(otherTeam, otherQuestion);
+
 		return found.map(answer -> answer.getPercentValue()).orElse(BigDecimal.ZERO);
-	}	
+	}
+
+	public String peopleCount(CompanyTeam otherTeam, Question otherQuestion) {
+		Optional<ReportPerTeamAnswer> found = findAnswer(otherTeam, otherQuestion);
+		BigDecimal totalCount = new BigDecimal(
+				found.map(answer -> answer.getPeopleCount()).orElse(0));
+
+		int totalAnswers = totalCount.multiply(percentCount(otherTeam, otherQuestion),
+				new MathContext(2, RoundingMode.UP)).intValue();
+
+		return totalAnswers + "/" + totalCount;
+	}
 
 }
