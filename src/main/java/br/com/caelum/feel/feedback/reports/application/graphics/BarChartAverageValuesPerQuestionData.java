3b837d7142c1cfeef06d09baec9e61dbdb56f611
asouza <alots.ssa@gmail.com>
2018-10-22 19:56:55 -0200

    colocando cores legais

--- src/main/java/br/com/caelum/feel/feedback/reports/application/graphics/BarChartAverageValuesPerQuestionData.java
+++ src/main/java/br/com/caelum/feel/feedback/reports/application/graphics/BarChartAverageValuesPerQuestionData.java
@@ -10,7 +10,7 @@ public class BarChartAverageValuesPerQuestionData<T> {
 	private List<String[]> labels = new ArrayList<>();
 	private List<BigDecimal> values = new ArrayList<>();
 	private List<String> colors = new ArrayList<>();
-	private String[] colorsOptions = {"red","green" ,"orange","yellow","purple","gray","pink","#ff9999","ffb399","ffcc99","ffe699","ffff99","e6ff99","ccff99","99ffff","9999ff","b399ff","cc99ff","e699ff","ff99ff","ff99e6","ff99cc","ff99b3","ff9999","fff2e6","4d2600","000000"};
+	private String[] colorsOptions = {"#f73232","#ffa016" ,"#fff002","#4bf84c","#3ec714","#95f442","pink","#ff9999","#ffb399","#ffcc99","#ffe699","#ffff99","#e6ff99","#ccff99","#99ffff","#9999ff","#b399ff","#cc99ff","#e699ff","#ff99ff","#ff99e6","#ff99cc","#ff99b3","#ff9999","#fff2e6","#4d2600","#000000"};
 
 	public BarChartAverageValuesPerQuestionData(List<T> results,Function<T, GraphicData> mapper) {
 		
