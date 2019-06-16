c498e85fc8d0815b786d92d2f3fc085e5854e4db
asouza <alots.ssa@gmail.com>
2019-06-16 06:41:48 -0300

    diferenciando producao e dev no envio do email

--- src/main/java/br/com/caelum/feel/infra/MandrillMailer.java
+++ src/main/java/br/com/caelum/feel/infra/MandrillMailer.java
@@ -2,21 +2,33 @@ package br.com.caelum.feel.infra;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class MandrillMailer {
+	
+	@Autowired
+	private Environment env;
 
 	@Async
 	public void send(String subject, String rawText, String from, String fromName,
 			List<String> emailsToSend) {
 
-		MandrillMailDTO mail = new MandrillMailDTO(subject, rawText, from, fromName, emailsToSend);
-		MandrillMessageDTO message = new MandrillMessageDTO(mail);
-		new RestTemplate().postForEntity("https://mandrillapp.com/api/1.0/messages/send.json",
-				message, String.class);
+		
+		//aqui poderiam ser duas clasess, funcoes de producao e tal... mas desse jeito ta de boa por enquanto.
+		if(env.acceptsProfiles("production")) {		
+			MandrillMailDTO mail = new MandrillMailDTO(subject, rawText, from, fromName, emailsToSend);
+			MandrillMessageDTO message = new MandrillMessageDTO(mail);
+			new RestTemplate().postForEntity("https://mandrillapp.com/api/1.0/messages/send.json",
+					message, String.class);
+		} else {
+			System.out.println("Enviando emails para "+emailsToSend);
+			System.out.println("Conteúdo:"+rawText);
+		}
 	}
 
 }
